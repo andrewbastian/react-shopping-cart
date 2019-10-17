169a4fdf63940abfcdcc7ac7a5b5b3b6e61b4d94
andrewbastian <andrew.bastian@gmail.com>
2019-10-16 17:26:36 -0700

    @ step 4. Everything working so far.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+
+import { ProductContext } from './contexts/ProductContext';
+//import { CartContext } from './contexts/CartContext'
+
+
 function App() {
+
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
@@ -16,6 +22,8 @@ function App() {
 	};
 
 	return (
+		<ProductContext.Provider value={{ products, addItem }}>
+
 		<div className="App">
 			<Navigation cart={cart} />
 
@@ -23,19 +31,15 @@ function App() {
 			<Route
 				exact
 				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
+				component={Products}
 			/>
 
 			<Route
 				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
+				component={ShoppingCart}
 			/>
 		</div>
+	</ProductContext.Provider>
 	);
 }
 
